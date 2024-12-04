added fix for attention mask error (???)

pre-trained-model/train.py
@@ -1,11 +1,14 @@
-from datasets import Dataset, concatenate_datasets, Value, Sequence
-from transformers import (AutoModelForSeq2SeqLM, AutoTokenizer, Seq2SeqTrainer,
-                          Seq2SeqTrainingArguments)
-from transformers import DataCollatorForSeq2Seq
+import csv
+import random
+
 import evaluate
 import numpy as np
 import torch
-import random
+from datasets import (Dataset, Sequence, Value, concatenate_datasets,
+                      load_dataset)
+from transformers import (AutoModelForSeq2SeqLM, AutoTokenizer,
+                          DataCollatorForSeq2Seq, Seq2SeqTrainer,
+                          Seq2SeqTrainingArguments)
 
 
 def train_model(
@@ -44,39 +47,53 @@ def train_model(
     source_to_target_data = parallel_data
     target_to_source_data = parallel_data
 
-    source_to_target_prefix = f"Translate {source_lang} to {target_lang}: "
-    target_to_source_prefix = f"Translate {target_lang} to {source_lang}: "
+    # source_to_target_prefix = f"Translate {source_lang} to {target_lang}: "
+    # target_to_source_prefix = f"Translate {target_lang} to {source_lang}: "
 
-    source_to_target_data = source_to_target_data.map(lambda x: {source_lang: source_to_target_prefix + x[source_lang]})
-    target_to_source_data = target_to_source_data.map(lambda x: {target_lang: target_to_source_prefix + x[target_lang]})
+    # don't include prefixes
+    # when training, the model will predict the prefix
+    # huggingface probably has a way to fix this
+
+    # source_to_target_data = source_to_target_data.map(lambda x: {source_lang: source_to_target_prefix + x[source_lang]})
+    # target_to_source_data = target_to_source_data.map(lambda x: {target_lang: target_to_source_prefix + x[target_lang]})
 
     source_to_target_data = source_to_target_data.map(
-        preprocess_source_to_target_function,
+        preprocess_source_to_target,
         batched=True,
+        fn_kwargs={
+            "source_lang": source_lang,
+            "target_lang": target_lang,
+        },
     )
     target_to_source_data = source_to_target_data.map(
-        preprocess_target_to_source_function,
+        preprocess_source_to_target,
         batched=True,
+        fn_kwargs={
+            "source_lang": target_lang,
+            "target_lang": source_lang,
+        },
     )
 
-    source_to_target_data = source_to_target_data.remove_columns(source_lang).remove_columns(target_lang)
-    target_to_source_data = target_to_source_data.remove_columns(source_lang).remove_columns(target_lang)
+    print(target_to_source_data)
+    print(source_to_target_data)
+
+    # source_to_target_data = source_to_target_data.remove_columns(
+    #     source_lang
+    # ).remove_columns(target_lang)
+    # target_to_source_data = target_to_source_data.remove_columns(
+    #     source_lang
+    # ).remove_columns(target_lang)
+
+    # source_data = source_data.map(lambda x: {source_lang: source_to_target_prefix + x[source_lang]})
+    # target_data = target_data.map(lambda x: {target_lang: target_to_source_prefix + x[target_lang]})
 
     # prepare monolingual data
-    source_data = source_data.map(lambda x: {source_lang: source_to_target_prefix + x[source_lang]})
-    target_data = target_data.map(lambda x: {target_lang: target_to_source_prefix + x[target_lang]})
 
-    source_data = source_data.map(
-        preprocess_source_function,
-        batched=True
-    )
-    target_data = target_data.map(
-        preprocess_target_function,
-        batched=True
-    )
+    source_data = source_data.map(preprocess_source_function, batched=True)
+    target_data = target_data.map(preprocess_target_function, batched=True)
 
-    source_data = source_data.remove_columns(source_lang)
-    target_data = target_data.remove_columns(target_lang)
+    # source_data = source_data.remove_columns(source_lang)
+    # target_data = target_data.remove_columns(target_lang)
 
     data_collator = DataCollatorForSeq2Seq(tokenizer, model=source_to_target_model)
 
@@ -104,7 +121,9 @@ def train_model(
         fp16=True,
     )
 
-    combined_target_to_source_data = target_to_source_data.train_test_split(test_size=0.1)
+    combined_target_to_source_data = target_to_source_data.train_test_split(
+        test_size=0.1
+    )
 
     for _ in range(iterations):
         target_to_source_trainer = Seq2SeqTrainer(
@@ -113,40 +132,72 @@ def train_model(
             train_dataset=combined_target_to_source_data["train"],
             eval_dataset=combined_target_to_source_data["test"],
             data_collator=data_collator,
-            processing_class=tokenizer,
+            tokenizer=tokenizer,
             compute_metrics=compute_metrics,
         )
 
         target_to_source_trainer.train()
 
-        synthetic_source_data = target_to_source_trainer.predict(target_data, max_length=60).predictions.tolist()
+        print(target_data)
+        synthetic_source_data = target_to_source_trainer.predict(
+            test_dataset=target_data, max_length=40
+        ).predictions.tolist()
 
-        num_samples = 5 
+        num_samples = 5
 
         random_indices = random.sample(range(len(synthetic_source_data)), num_samples)
 
         random_pairs = [
-            (synthetic_source_data[idx], target_data["input_ids"][idx]) for idx in random_indices
+            (synthetic_source_data[idx], target_data["input_ids"][idx])
+            for idx in random_indices
         ]
 
         for i, (synthetic, target) in enumerate(random_pairs):
-            synthetic = [synthetic_token for synthetic_token in synthetic if synthetic_token != -100]
+            synthetic = [
+                synthetic_token
+                for synthetic_token in synthetic
+                if synthetic_token != -100
+            ]
+
             synthetic_tokens = tokenizer.decode(synthetic)
             target_tokens = tokenizer.decode(target)
+
             synthetic_sequence = "".join(synthetic_tokens)
             target_sequence = "".join(target_tokens)
+
             print(f"Pair {i + 1}:")
             print(f"Synthetic Source: {synthetic_sequence}")
             print(f"Target: {target_sequence}")
             print("-" * 50)
 
-        synthetic_source_to_target_data = target_data.rename_column("input_ids", "labels")
-        synthetic_source_to_target_data = synthetic_source_to_target_data.add_column("input_ids", synthetic_source_data)
+        synthetic_source_to_target_data = target_data.rename_column(
+            "input_ids", "labels"
+        )
+        synthetic_source_to_target_data = synthetic_source_to_target_data.add_column(
+            "input_ids",
+            synthetic_source_data,
+        )
 
-        synthetic_source_to_target_data = synthetic_source_to_target_data.cast_column("labels", Sequence(Value("int64")))
-        synthetic_source_to_target_data = synthetic_source_to_target_data.cast_column("input_ids", Sequence(Value("int32")))
+        synthetic_source_to_target_data.map(
+            fix_attention_mask,
+            batched=True,
+        )
 
-        combined_source_to_target_data = concatenate_datasets([source_to_target_data, synthetic_source_to_target_data])
+        synthetic_source_to_target_data = synthetic_source_to_target_data.cast_column(
+            "labels", Sequence(Value("int64"))
+        )
+        synthetic_source_to_target_data = synthetic_source_to_target_data.cast_column(
+            "input_ids", Sequence(Value("int32"))
+        )
+
+        combined_source_to_target_data = concatenate_datasets(
+            [source_to_target_data, synthetic_source_to_target_data]
+        )
+
+        print(combined_source_to_target_data["input_ids"][:10])
+        print(combined_source_to_target_data["attnetion_mask"][:10])
+        print(combined_source_to_target_data["labels"][:10])
+        print(combined_source_to_target_data)
 
         for datapoint in combined_source_to_target_data:
             if len(datapoint["input_ids"]) != len(datapoint["attention_mask"]):
@@ -154,62 +205,103 @@ def train_model(
         #
         # print(combined_source_to_target_data["input_ids"])
         # print(combined_source_to_target_data["attention_mask"])
-        
-        combined_source_to_target_data = combined_source_to_target_data.train_test_split(test_size=0.1)
-        
+
+        combined_source_to_target_data = (
+            combined_source_to_target_data.train_test_split(test_size=0.1)
+        )
+
         source_to_target_trainer = Seq2SeqTrainer(
             model=source_to_target_model,
             args=source_to_target_training_args,
             train_dataset=combined_source_to_target_data["train"],
             eval_dataset=combined_source_to_target_data["test"],
             data_collator=data_collator,
-            processing_class=tokenizer,
+            tokenizer=tokenizer,
             compute_metrics=compute_metrics,
         )
 
         source_to_target_trainer.train()
 
-        synthetic_target_data = source_to_target_trainer.predict(source_data).predictions.astype(np.int64).tolist()
+        synthetic_target_data = (
+            source_to_target_trainer.predict(source_data)
+            .predictions.astype(np.int64)
+            .tolist()
+        )
 
-        synthetic_target_to_source_data = source_data.rename_column("input_ids", "labels")
-        synthetic_target_to_source_data = synthetic_target_to_source_data.add_column("input_ids", synthetic_target_data)
+        synthetic_target_to_source_data = source_data.rename_column(
+            "input_ids", "labels"
+        )
+        synthetic_target_to_source_data = synthetic_target_to_source_data.add_column(
+            "input_ids", synthetic_target_data
+        )
 
-        combined_target_to_source_data = concatenate_datasets([target_to_source_data, synthetic_target_to_source_data])
+        combined_target_to_source_data = concatenate_datasets(
+            [target_to_source_data, synthetic_target_to_source_data]
+        )
 
-        combined_source_to_target_data = combined_target_to_source_data.train_test_split(test_size=0.1)
+        combined_source_to_target_data = (
+            combined_target_to_source_data.train_test_split(test_size=0.1)
+        )
 
     return source_to_target_model, target_to_source_model
 
 
+def fix_attention_mask(examples):
+    new_attention_mask = [1 for x in examples["input_ids"] if x != -100]
+
+    examples["attention_mask"] = new_attention_mask
+
+    return examples
+
+
+def preprocess_source_to_target(examples, source_lang, target_lang, max_length=200):
+    inputs = examples[source_lang]
+    targets = examples[target_lang]
+
+    model_inputs = tokenizer(
+        inputs, text_target=targets, max_length=max_length, truncation=True
+    )
+
+    return model_inputs
+
+
 def preprocess_source_to_target_function(examples, max_length=200):
-    inputs = examples[src_lang] 
+    inputs = examples[src_lang]
     targets = examples[tgt_lang]
     model_inputs = tokenizer(
         inputs, text_target=targets, max_length=max_length, truncation=True
     )
     return model_inputs
 
+
 def preprocess_target_to_source_function(examples, max_length=200):
-    inputs = examples[tgt_lang] 
+    inputs = examples[tgt_lang]
     targets = examples[src_lang]
     model_inputs = tokenizer(
         inputs, text_target=targets, max_length=max_length, truncation=True
     )
     return model_inputs
 
+
 def preprocess_target_function(examples, max_length=200):
-    inputs = examples[tgt_lang] 
-    model_inputs = tokenizer(
-        inputs, max_length=max_length, truncation=True
-    )
+    inputs = examples[tgt_lang]
+    model_inputs = tokenizer(inputs, max_length=max_length, truncation=True)
     return model_inputs
 
+
 def preprocess_source_function(examples, max_length=200):
-    inputs = examples[src_lang] 
-    model_inputs = tokenizer(
-        inputs, max_length=max_length, truncation=True
-    )
+    inputs = examples[src_lang]
+    model_inputs = tokenizer(inputs, max_length=max_length, truncation=True)
     return model_inputs
+
+
+def yield_csv_lines(csv_dataset_path, source_lang, target_lang):
+    with open(csv_dataset_path, "r") as csv_file:
+        filereader = csv.reader(csv_file)
+        for line in filereader:
+            if len(line[0]) == 30:
+                yield {source_lang: line[0], target_lang: line[1]}
+
 
 def yield_paired_lines(source_path, target_path, source_lang, target_lang):
     with open(source_path, "r", encoding="utf-8") as source_text_file, open(
@@ -224,93 +316,6 @@ def yield_mono_lines(path, lang):
         for line in file:
             yield {lang: line}
 
-
-def translate(translation_model, monolingual_data, tokenizer):
-    """
-    Creates synthetic data using a translation model and monolingual dataset
-
-    Parameters
-    ==========
-    translation_model: Dataset
-        pretrained model that can translate sentences from source language into sentences from target language
-
-    monolingual_data: Dataset
-        list of dictionaries with item {lang: text}
-
-    tokenizer: torch tokenizer?
-        object that can tokenize list of text
-
-    source_lang: str
-        the source language
-
-    target_lang: str
-        the target language
-
-    Returns
-    =======
-        returns a HuggingFace Dataset using the following dictionary {lang: [lines of translated text]}
-    """
-    # might need to prefix each line of the data with f'translate {source_language} to {target_language}'
-    # assume the data does not have translation prompt prefix
-    # what data type is the monolingual data, dictionary? list? HuggingFace Dataset?
-
-    # translates the tokens from the source language to tokens in the target language
-    # top 5 tokens retained
-    input_ids = torch.Tensor(monolingual_data["input_ids"])
-    output_tokens = translation_model.generate(
-        input_ids, max_new_tokens=40, top_k=5, top_p=0.95
-    )
-    # decode the generated token ids back into text from the target language
-    # not sure if I should only index first element, might change this code later once we get data
-    synthetic_data = tokenizer.decode(output_tokens[0], skip_special_tokens=True)
-
-    return synthetic_data
-
-
-def combine(source_data, target_data, parallel_data, source_lang, target_lang):
-    """
-    Creates a new HuggingFace Dataset with the previous parallel data and adds on
-    the paired source and synthetic target data
-
-    Paramter
-    ========
-    source_data: dict, source_lang: source_text
-        dictionary of sentences from the source language
-
-    synthetic_target_data: dict, target_lang: target_text
-        dictionary of sentences from the target language
-
-    parallel_data: dict, source_lang: source_text, target_lang: target_lang
-        HuggingFace dictionary of paired sentences from the source and target languages
-
-    Returns
-    =======
-        HuggingFace dataset with the keys, values items source_lang: source_text, target_lang: target_text
-    """
-    # I assumed the data will look like the following:
-    # source_dataset => {lang: line}
-    # synthetic_target_data => {lang: line}
-    # parallel_dataset => {source_lang: source_line, target_lang: target_line}
-
-    # can't append to a HuggingFace Dataset, immutable (uses similar interface to an array),
-    # have to create a new dataset object
-
-    assert len(target_data) == len(source_data)
-
-    source_lines = [source_dict[source_lang] for source_dict in source_data]
-    target_lines = [target_dict[target_lang] for target_dict in target_data]
-
-    for paired_dict in parallel_data:
-        source_text = paired_dict[source_lang]
-        target_text = paired_dict[target_lang]
-
-        source_lines.append(source_text)
-        target_lines.append(target_text)
-
-    parallel_dictionary = {source_lang: source_lines, target_lang: target_lines}
-    combined_parallel_data = Dataset.from_dict(parallel_dictionary)
-
-    return combined_parallel_data
 
 def compute_metrics(eval_preds):
     preds, labels = eval_preds
@@ -331,6 +336,7 @@ def compute_metrics(eval_preds):
     result = metric.compute(predictions=decoded_preds, references=decoded_labels)
     return {"bleu": result["score"]}
 
+
 if __name__ == "__main__":
     tokenizer = AutoTokenizer.from_pretrained("google-t5/t5-small")
     source_to_target_model = AutoModelForSeq2SeqLM.from_pretrained("google-t5/t5-small")
@@ -339,14 +345,32 @@ if __name__ == "__main__":
     src_lang = "AAVE"
     tgt_lang = "SAE"
 
-    paired_src_data_path = f"/content/gdrive/MyDrive/6.861 Project/data/AAVE-SAE-data/{src_lang}_samples.txt"
-    paired_tgt_data_path = f"/content/gdrive/MyDrive/6.861 Project/data/AAVE-SAE-data/{tgt_lang}_samples.txt"
+    # used for paired txt files
+    #
+    # paired_src_data_path = f"/content/gdrive/MyDrive/6.861 Project/data/AAVE-SAE-data/{src_lang}_samples.txt"
+    # paired_tgt_data_path = f"/content/gdrive/MyDrive/6.861 Project/data/AAVE-SAE-data/{tgt_lang}_samples.txt"
+    #
+    # raw_paired_dataset = Dataset.from_generator(
+    #     yield_paired_lines,
+    #     gen_kwargs={
+    #         "source_path": paired_src_data_path,
+    #         "target_path": paired_tgt_data_path,
+    #         "source_lang": src_lang,
+    #         "target_lang": tgt_lang,
+    #     },
+    # )
+
+    # used for one csv file
+
+    paired_csv_data_path = "/content/gdrive/MyDrive/6.861 Project/data/AAVE-SAE-data/GPT Translated AAVE Lyrics.csv"
+    # paired_csv_data_path = (
+    #     "/Users/willreed/nlp-final-project/GPT-Translated-AAVE-Lyrics.csv"
+    # )
 
     raw_paired_dataset = Dataset.from_generator(
-        yield_paired_lines,
+        yield_csv_lines,
         gen_kwargs={
-            "source_path": paired_src_data_path,
-            "target_path": paired_tgt_data_path,
+            "csv_dataset_path": paired_csv_data_path,
             "source_lang": src_lang,
             "target_lang": tgt_lang,
         },
@@ -354,6 +378,8 @@ if __name__ == "__main__":
 
     monolingual_src_data_path = "/content/gdrive/MyDrive/6.861 Project/data/AAVE-SAE-data/coraal_dataset.txt"
     monolingual_tgt_data_path = "/content/gdrive/MyDrive/6.861 Project/data/AAVE-SAE-data/cleaned_BAWE.txt"
+    # monolingual_src_data_path = "/Users/willreed/nlp-final-project/coraal_dataset.txt"
+    # monolingual_tgt_data_path = "/Users/willreed/nlp-final-project/cleaned_BAWE.txt"
 
     raw_monolingual_src_data = Dataset.from_generator(
         yield_mono_lines,
@@ -363,10 +389,21 @@ if __name__ == "__main__":
         yield_mono_lines,
         gen_kwargs={"path": monolingual_tgt_data_path, "lang": tgt_lang},
     )
-    
-    raw_monolingual_tgt_data = raw_monolingual_tgt_data.filter(lambda x: len(x['SAE']) > 50 and len(x['SAE']) < 65)
+
+    raw_monolingual_tgt_data = raw_monolingual_tgt_data.filter(
+        lambda x: len(x["SAE"]) > 50 and len(x["SAE"]) < 65
+    )
 
     metric = evaluate.load("sacrebleu")
 
-    train_model(raw_paired_dataset, source_to_target_model, target_to_source_model, tokenizer, raw_monolingual_src_data, raw_monolingual_tgt_data, 1, src_lang, tgt_lang)
-
+    train_model(
+        raw_paired_dataset,
+        source_to_target_model,
+        target_to_source_model,
+        tokenizer,
+        raw_monolingual_src_data,
+        raw_monolingual_tgt_data,
+        1,
+        src_lang,
+        tgt_lang,
+    )