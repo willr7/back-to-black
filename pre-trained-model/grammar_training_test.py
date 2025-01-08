a1cac3ef098222a5ba49f578e5e163d5fc1f7e62
Will Reed <willr343@mit.edu>
2025-01-08 12:49:43 -0500

    some python language tool tests

--- pre-trained-model/grammar_training_test.py
+++ pre-trained-model/grammar_training_test.py
@@ -1,10 +1,13 @@
 from language_tool_python import LanguageTool
 
-tool = LanguageTool('en-US')
+tool = LanguageTool("en-US")
 
 s = "Y'all ain't gon' believe this."
 
 matches = tool.check(s)
 
 print(len(matches))
-
+print(matches)
+print(matches[0])
+print(matches[1])
+print(matches[0])
